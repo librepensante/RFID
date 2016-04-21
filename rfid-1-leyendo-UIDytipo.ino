18cc724cf612d48d377cee8352ac1093cfe30894
librepensante <thingco@gmail.com>
2016-04-21 09:54:18 -0500

    Update rfid-1-leyendo-UIDytipo.ino

--- rfid-1-leyendo-UIDytipo.ino
+++ rfid-1-leyendo-UIDytipo.ino
@@ -1,32 +1,29 @@
-#include <SPI.h>
-#include <MFRC522.h>
+#include <SPI.h> // Serial Perispherical Interface  
+#include <MFRC522.h> // lector/escritor RC522
 
 #define SS_PIN 10
 #define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // MFRC522 instancia.
 
 void setup() {
-    Serial.begin(9600); // Initialize serial communications with the PC
-    SPI.begin();            // Init SPI bus
-    mfrc522.PCD_Init(); // Init MFRC522 card
-    Serial.println("Scan PICC to see UID and type...");
+    Serial.begin(9600); // Comunicación serial 
+    SPI.begin();            // comunicación SPI
+    mfrc522.PCD_Init(); // inicia MFRC522
+    Serial.println("Escaniando: UID y tipo...");
 }
 
 void loop() {
-    // Look for new cards
+    // busca nuevas tarjetas
     if ( ! mfrc522.PICC_IsNewCardPresent()) {
         return;
     }
 
-    // Select one of the cards
+    // Selecciona una 
     if ( ! mfrc522.PICC_ReadCardSerial()) {
         return;
     }
 
-    // Dump debug info about the card. PICC_HaltA() is automatically called.
+    // busca la información de la tarjeta y termina el llamado.
   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-//Serial.println(mfrc522.uid.uidByte[0]);
-delay(1000);
-    
       }
     
